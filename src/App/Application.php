9f3e465b52c0cb68be6c988d673ebf0e04fb317f
Charaf Rezrazi <2086576+Rezrazi@users.noreply.github.com>
2021-02-12 12:21:43 +0100

    fix(application.php): changed path resolution
    this solves compatibility issues with windows/linux environments

--- src/App/Application.php
+++ src/App/Application.php
@@ -5,11 +5,11 @@ namespace App;
 class Application extends \Illuminate\Foundation\Application
 {
     protected $namespace = 'App\\';
-    protected $databasePath = 'src\\App\\Database';
-    protected $appPath = 'src\\App';
+    protected $databasePath = __DIR__ . '/Database';
+    protected $appPath = __DIR__;
 
     public function resourcePath($path = '')
     {
-        return $this->appPath.DIRECTORY_SEPARATOR.'Resources'.($path ? DIRECTORY_SEPARATOR.$path : $path);
+        return $this->appPath . DIRECTORY_SEPARATOR . 'Resources' . ($path ? DIRECTORY_SEPARATOR . $path : $path);
     }
 }
